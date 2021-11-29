d2bacc111e66e483c615fd4e789de145d63c07e6
Arvind <arbindudebnath59985@gmail.com>
2021-11-29 19:42:46 +0600

    our story section done

--- src/components/Pages/Home/OurStory.js
+++ src/components/Pages/Home/OurStory.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const OurStory = () => {
     return (
-        <div>
-            our story
+        <div className='text-center'>
+            <h6 className='text-yellow-500 font-bold text-base'>OUR STORY</h6>
+            <h2 className='text-4xl font-bold mt-2 mb-1'>About our restaurant</h2>
+
+            <p className='hidden md:block text-lg font-base text-gray-500 mt-2'>Every day, more than 1 million guests visit our restaurants around the world. <br />And they do so because our restaurants are known for serving high-quality, <br /> great-tasting, and affordable food.The original Home of the Whopper, our<br />commitment premium ingredients,recipes, and family-friendly dining.</p>
+
+            <p className='md:hidden text-justify mx-5 text-lg font-base text-gray-500 mt-3'>
+            Every day, more than 1 million guests visit our restaurants around the world.  And they do so because our restaurants are known for serving high-quality, great-tasting, and affordable food.The original Home of the Whopper, our commitment to premium ingredients, signature recipes, and family-friendly dining.
+            </p>
+
+            <button className='bg-yellow-500 p-2 px-7 mr-3 mt-5 text-white font-bold hover:bg-yellow-600'>Discover More</button>
         </div>
     );
 };
--- src/components/Pages/Home/TodaysFeature.js
+++ src/components/Pages/Home/TodaysFeature.js
@@ -11,7 +11,7 @@ const TodaysFeature = () => {
                 <h5 className='text-lg text-yellow-500 font-bold'>TODAY'S MAIN DISH</h5>
                 <h1 className='text-4xl font-bold mt-2 mb-1'>Vegetable Omelete</h1>
                 <p className='text-lg font-base text-gray-500 hidden md:block'>World’s Best Vegetarian Omelette, for a healthy and happy start of your day. <br/> Cooked with broccoli, eggs and sun-dried tomatoes.You’d be surprised how this omelette taste.</p>
-                <p className='text-lg font-base text-gray-500 md:hidden'>World's Best Vegetaritan Omelette. For a healthy and happy start of your day.</p>
+                <p className='text-lg font-base text-gray-500 md:hidden'>World's Best Vegetarian Omelette. For a healthy and happy start of your day.</p>
                 <div className="mt-8">
                     <button className='bg-yellow-500 p-2 px-7 mr-3 text-white font-bold hover:bg-yellow-600'>Book a table</button>
                     <button className='border border-yellow-500 p-1.5 px-7 font-bold hover:bg-yellow-500 hover:text-white'>Discover more</button>

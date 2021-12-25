5f30947cd4e1d7c288aa3fa58bb6d83c40224f46
Arvind <arbindudebnath59985@gmail.com>
2021-12-25 19:34:10 +0600

    added social icon on chef

--- src/components/Pages/Home/Chef.js
+++ src/components/Pages/Home/Chef.js
@@ -21,10 +21,15 @@ const Chef = () => {
             </div>
             <div className='flex justify-center text-center mx-1'>
                 {
-                    chef.map(chef => <div key={chef._id} className='hover:border-yellow-500 border-2 border-transparent hover:bg-yellow-500 hover:text-white pb-12'>
+                    chef.map(chef => <div key={chef._id} className='hover:border-yellow-500 border-2 border-transparent hover:bg-yellow-500 pb-12'>
                         <img src={chef.img} alt="" />
                         <h1 className='md:font-bold font-semibold md:text-lg mt-5'>{chef.name}</h1>
                         <h1 className='md:font-medium text-gray-600'>{chef.Detail}</h1>
+                        <div className='mt-4 cursor-pointer'>
+                            <i class="fab fa-facebook mx-2 hover:bg-white bg-yellow-500 rounded-full p-2"></i>
+                            <i class="fab fa-instagram-square mx-2 hover:bg-white bg-yellow-500 rounded-full p-2"></i>
+                            <i class="fab fa-twitter mx-2 hover:bg-white bg-yellow-500 rounded-full p-2"></i>
+                        </div>
                     </div>)
                 }
             </div>

60c98b1c36a75120bc628b12cb4cb726d1b15d2e
Arvind <arbindudebnath59985@gmail.com>
2022-01-18 03:10:17 +0600

    complete about page with responsiveness

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -25,27 +25,27 @@ const About = () => {
                 </div>
                 <hr className='lg:mx-72 md:mx-10 mx-5 my-7 border-8 border-gray-100 rounded-full' />
 
-                <div className='lg:mx-72 grid lg:grid-cols-3 md:grid-cols-2 pt-20 pb-48'>
+                <div className='lg:mx-72 md:mx-10 mx-2 grid lg:grid-cols-3 md:grid-cols-2 pt-20 md:pb-48 pb-16'>
                     <div>
                         <img style={{height:'450px', width:'100%', objectFit:'cover'}} src={about1} alt="" />
                     </div>
-                    <div className='flex flex-col justify-center items-center'>
+                    <div className='flex flex-col justify-center items-center md:my-0 my-20'>
                         <img src={cutlery} alt="" />
                         <h3 className='text-center font-bold text-2xl mt-2'>Quality food</h3>
                         <h6 className='text-center px-16 mt-3 font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit doloremque cum optio, repudiandae assumenda maiores!</h6>
                     </div>
-                    <div>
+                    <div className='lg:mt-0 mt-3'>
                         <img style={{height:'450px', width:'100%', objectFit:'cover'}} src={about2} alt="" />
                     </div>
-                    <div className='flex flex-col justify-center items-center'>
+                    <div className='flex flex-col justify-center items-center lg:mt-0 md:mt-3 my-20'>
                         <img src={invoice} alt="" />
                         <h3 className='text-center font-bold text-2xl mt-2'>Original recipes</h3>
                         <h6 className='text-center px-16 mt-3 font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, corporis repudiandae. In dolorem accusamus perferendis!</h6>
                     </div>
-                    <div>
+                    <div className='lg:mt-0 mt-3'>
                         <img style={{height:'450px', width:'100%', objectFit:'cover'}} src={about3} alt="" />
                     </div>
-                    <div className='flex flex-col justify-center items-center'>
+                    <div className='flex flex-col justify-center items-center lg:mt-0 md:mt-3 md:my-0 my-20'>
                         <img src={clock} alt="" />
                         <h3 className='text-center font-bold text-2xl mt-2'>Fastest delivery</h3>
                         <h6 className='text-center px-16 mt-3 font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab optio porro, magni provident inventore quibusdam.</h6>
@@ -54,7 +54,7 @@ const About = () => {
                 <div className='text-center mb-48'>
                     <h6 className='text-yellow-500 font-bold'>OUR VISION</h6>
                     <h2 className='text-4xl mt-2 mb-12 font-bold'>Our philosophy</h2>
-                    <p className='px-72 lg:mx-72 text-2xl text-gray-600 mb-6'><span className="text-yellow-500 text-5xl font-bold">L</span>orem ipsum dolor sit amet consectetur, adipisicing elit. Sed suscipit fugit dolorum eaque ab corrupti, consequuntur earum porro temporibus nemo cum quasi aspernatur dignissimos repellat, assumenda, beatae quas incidunt laboriosam.</p>
+                    <p className='lg:px-72 lg:mx-72 md:px-32 text-2xl text-gray-600 mb-6'><span className="text-yellow-500 text-5xl font-bold">L</span>orem ipsum dolor sit amet consectetur, adipisicing elit. Sed suscipit fugit dolorum eaque ab corrupti, consequuntur earum porro temporibus nemo cum quasi aspernatur dignissimos repellat, assumenda, beatae quas incidunt laboriosam.</p>
                     <h6 className='text-xl font-bold mb-1'>Arvind Debnath</h6>
                     <h6 className='text-xm font-semibold text-yellow-500'>Spicy Restaurant CEO</h6>
                 </div>
